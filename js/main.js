76d925f007e283ed20cc89917efa410559521d70
Jon Rutishauser <jrutishauser@gmail.com>
2015-03-01 19:23:58 -0500

    task on list when checked gets crossed off and turns red

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+(function () { 
+	angular.module('taskfire', ['firebase'])
+		.controller('TaskController', function($firebase) { 
+			var ref = new Firebase('https://taskfire.firebaseio.com/tasklist');	
+				ref.set({
+					task: 'learn firebase',
+					completed: false 
+				});			
+			this.todoItems = [
+				{task:'learn firebase', completed: false},
+				{task:'play with angular', completed: false}
+			];	
+		
+		
+		
+		
+		})
 
+
+
+
+
+
+; //end module
+})();
