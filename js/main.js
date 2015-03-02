0e11e603488653f74d9f214643bff2cce1035287
Jon Rutishauser <jrutishauser@gmail.com>
2015-03-02 15:24:28 -0500

    task list and firebase are talking for adding items. no method to remove yet, and check only works local

--- js/main.js
+++ js/main.js
@@ -3,21 +3,13 @@
 		.controller('TaskController', function($firebase) { 
 			var ref = new Firebase('https://taskfire.firebaseio.com/tasklist');	
 			var sync = $firebase(ref);	
-			this.todoItems = [
-			 ];
-			 ref.on('value', function(snapshot){
-				var remTodoItems = (snapshot.val());
-				remTodoItems = remTodoItems.val();
-				console.log(remTodoItems.key());
-			   	todoItems = remTodoItems;
-				console.log(this.todoItems);	
-			 });
+			this.todoItems = {};
 			this.addItem = function(newTodoItem){
-				this.todoItems.push({task: this.newTodoItem, completed: false});	
 				sync.$push({task: this.newTodoItem, completed: false});	
 				this.newTodoItem = "";
+					
 			};	
-			// console.log(this.todoItems);	
+			this.todoItems = sync.$asObject();	
 		})
 
 
