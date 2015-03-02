b9d2c614f3fd86c4977041afb56b061b02f06771
Jon Rutishauser <jrutishauser@gmail.com>
2015-03-01 21:34:05 -0500

    still playing with firebase and adding items

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
-(function () { 
+(function () {
 	angular.module('taskfire', ['firebase'])
 		.controller('TaskController', function($firebase) { 
 			var ref = new Firebase('https://taskfire.firebaseio.com/tasklist');	
-				ref.set({
-					task: 'learn firebase',
-					completed: false 
-				});			
+			var sync = $firebase(ref);	
 			this.todoItems = [
 				{task:'learn firebase', completed: false},
-				{task:'play with angular', completed: false}
-			];	
-		
-		
-		
-		
+				{task:'play with angular', completed: true}
+			];
+			this.addItem = function(){
+				this.todoItems.push({text: this.newTodoItem, completed: false});	
+			};	
+			sync.$set(this.todoItems);	
+			console.log(this.todoItems);	
 		})
 
 
